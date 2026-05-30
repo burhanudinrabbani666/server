abe68b0af5fe34ada49d7331d0f7966021731d3a
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-30 22:29:13 +0700

    fixing dotenv

--- src/config/key.ts
+++ src/config/key.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
-const ENV = process.env["NODE_ENV"];
 
-dotenv.config({ path: ENV ? `.env.${ENV}` : ".env" });
+dotenv.config();
 
 function requireEnv(key: string): string {
     const value = process.env[key];
@@ -12,7 +11,7 @@ function requireEnv(key: string): string {
 }
 
 export const PORT = requireEnv("PORT");
-export const GOOGLE_CLIENT_ID = requireEnv("GOOLE_CLIENT_ID");
+export const GOOGLE_CLIENT_ID = requireEnv("GOOGLE_CLIENT_ID");
 export const GOOGLE_CLIENT_SECRET = requireEnv("GOOGLE_CLIENT_SECRET");
 export const MONGO_DB_URI = requireEnv("MONGO_DB_URI");
 export const SESSION_SECRET = requireEnv("SESSION_SECRET");
